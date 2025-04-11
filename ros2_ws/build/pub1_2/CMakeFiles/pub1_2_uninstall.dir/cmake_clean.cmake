1a4c1cb01c78f727af04ea72e0cfb1d39d7faca2
kim jae won <kjw2001174@gmail.com>
2025-04-11 10:24:36 +0900

    Delete ros2_ws directory

--- ros2_ws/build/pub1_2/CMakeFiles/pub1_2_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pub1_2_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pub1_2_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros2_ws/build/pub1_2/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/linux/ros2_ws/build/pub1_2/pub1_2" "TARGETS" "pub1_2" "DESTINATION" "lib/pub1_2")
