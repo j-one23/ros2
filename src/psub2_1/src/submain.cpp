d30c83eeb82942ff8f4866371af42b9c062fd695
kim jae won <kjw2001174@gmail.com>
2025-04-23 19:25:07 +0900

    Delete src/psub2_1 directory

--- src/psub2_1/src/submain.cpp
+++
@@ -1,9 +0,0 @@
-#include "psub2_1/sub.hpp"
-int main(int argc, char * argv[])
-{
-rclcpp::init(argc, argv);
-auto node = std::make_shared<Sub>();
-rclcpp::spin(node);
-rclcpp::shutdown();
-return 0;
-}
