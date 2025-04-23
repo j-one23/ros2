0fdc5e06ad1fae70d95d45fcaccaa5a2bb6b0086
kim jae won <kjw2001174@gmail.com>
2025-04-23 19:34:34 +0900

    Add files via upload

---
+++ src/psub2_2/src/sub.cpp
@@ -0,0 +1,11 @@
+#include "psub2_2/sub.hpp"
+void Sub::subscribe_msg(const geometry_msgs::msg::Vector3::SharedPtr msg) const
+{
+RCLCPP_INFO(this->get_logger(), "Received message: x : %lf, y : %lf, z : %lf'", msg->x,msg->y,msg->z);
+}
+Sub::Sub() : Node("mysub")
+{
+auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+sub_ = this->create_subscription<geometry_msgs::msg::Vector3>("mytopic", qos_profile,
+std::bind(&Sub::subscribe_msg, this, _1));
+}
